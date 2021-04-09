fb1656d6ef152018be01619796654374c5fae381
JSourianos <thomasourianos@live.no>
2021-04-09 10:23:34 +0200

    Splash screen implemented. HomeActivity next

--- app/src/main/java/com/kristiania/exam2021/MainActivity.kt
+++ app/src/main/java/com/kristiania/exam2021/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.kristiania.exam2021
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.core.os.HandlerCompat.postDelayed
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //This is the first screen, so we want to hide the top navigation bar describing our app
+        supportActionBar?.hide()
+
+        //We use the handler class to add a 3second delay to the MainActivity class
+        Handler().postDelayed({
+            //We send an Intent letting our app know that after 3 seconds, we want to switch to the home screen (main screen for our app)
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
