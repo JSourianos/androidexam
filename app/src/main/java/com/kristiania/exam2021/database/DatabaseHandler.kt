37c4092d3e0e13a676a0efd3abdb86e6553039dc
Sander Ulset <sanderulset1@gmail.com>
2021-04-29 23:56:24 +0200

    Refactor fordi jeg kan ikke kotlin

--- app/src/main/java/com/kristiania/exam2021/database/DatabaseHandler.kt
+++ app/src/main/java/com/kristiania/exam2021/database/DatabaseHandler.kt
@@ -3,10 +3,37 @@ package com.kristiania.exam2021.database
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns
 
-class DatabaseHandler (context: Context) : SQLiteOpenHelper (context, "ExamDB", null, 1) {
+object DatabaseInfo {
+    const val DB_VERSION = 1
+
+    object TableInfo: BaseColumns {
+        const val TABLE_NAME = "PurchasedCrypto"
+        const val COLUMN_PK = "BuyId"
+        const val COLUMN_CRYPTO_NAME = "CryptoName"
+        const val COLUMN_CRYPTO_PRICE = "Price"
+        const val COLUMN_CRYPTO_AMOUNT = "Amount"
+        const val COLUMN_CRYPTO_BUYDATE = "Date"
+    }
+
+
+    const val CREATE_TABLE_QUERY = """
+        CREATE TABLE ${TableInfo.TABLE_NAME} (
+        ${TableInfo.COLUMN_PK} INTEGER PRIMARY KEY AUTOINCREMENT,
+        ${TableInfo.COLUMN_CRYPTO_NAME} TEXT, 
+        ${TableInfo.COLUMN_CRYPTO_PRICE} INTEGER,
+        ${TableInfo.COLUMN_CRYPTO_AMOUNT} INTEGER,
+        ${TableInfo.COLUMN_CRYPTO_BUYDATE} TEXT
+    """
+
+    const val DELETE_TABLE_QUERY = "DROP TABLE IF EXISTS ${TableInfo.TABLE_NAME}"
+
+}
+
+class DatabaseHandler (context: Context) : SQLiteOpenHelper (context, "ExamDB", null, DatabaseInfo.DB_VERSION) {
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL("CREATE TABLE TEST (Test TEXT)")
+        db?.execSQL(DatabaseInfo.CREATE_TABLE_QUERY)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
