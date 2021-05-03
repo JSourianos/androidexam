1a4e15e8730e6fa0c914ee71b3a6a34f062e12a8
JSourianos <thomasourianos@live.no>
2021-05-03 18:49:10 +0200

    is all good! transactions implemented!!

--- app/src/main/java/com/kristiania/exam2021/TransactionActivity.kt
+++ app/src/main/java/com/kristiania/exam2021/TransactionActivity.kt
@@ -2,10 +2,16 @@ package com.kristiania.exam2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kristiania.exam2021.adapters.TransactionAdapter
+import com.kristiania.exam2021.database.PurchasedCryptoEntity
 import com.kristiania.exam2021.databinding.ActivityTransactionBinding
+import com.kristiania.exam2021.viewmodels.PortfolioViewModel
 
 class TransactionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTransactionBinding
+    private lateinit var viewModel: PortfolioViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +19,22 @@ class TransactionActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.hide() //hide nav
+
+        viewModel = PortfolioViewModel(this)
+
+        val transactionList = mutableListOf<PurchasedCryptoEntity>()
+        val adapter = TransactionAdapter(transactionList)
+        binding.rvTransactions.adapter = adapter
+        binding.rvTransactions.layoutManager = LinearLayoutManager(this)
+
+        viewModel.getListOfCryptos().observe(this){ownedCryptos ->
+            ownedCryptos.map {
+                transactionList.add(it)
+                Log.d("transactions: ", it.name)
+                adapter.notifyDataSetChanged()
+            }
+            adapter.notifyDataSetChanged()
+        }
+
     }
 }
---
+++ app/src/main/java/com/kristiania/exam2021/adapters/TransactionAdapter.kt
@@ -0,0 +1,34 @@
+package com.kristiania.exam2021.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kristiania.exam2021.database.PurchasedCryptoEntity
+import com.kristiania.exam2021.databinding.ItemCryptoBinding
+
+class TransactionAdapter(
+    var transactions: List<PurchasedCryptoEntity>
+) : RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder>() {
+
+
+    inner class TransactionViewHolder(val binding: ItemCryptoBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionViewHolder {
+        val binding =
+            ItemCryptoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TransactionViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TransactionViewHolder, position: Int) {
+        holder.itemView.apply {
+            holder.binding.itemCrypto.text = transactions[position].name
+            holder.binding.cryptoPrice.text = transactions[position].purchaseDate
+            holder.binding.cryptoPercent.text = transactions[position].amount.toString()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return transactions.size
+    }
+}
